8256dc2a7da4936f669d849d7f34bb7a33845fb0
YennNgoc <ngocvy.nts@gmail.com>
2021-06-19 17:19:01 +0700

    login initial

---
+++ Script/Role.sql
@@ -0,0 +1,19 @@
+select CURRENT_USER
+
+-- create ROLE
+CREATE ROLE HocVien
+CREATE ROLE NVQL_HocVien
+CREATE ROLE NVQL_LopHoc
+CREATE ROLE ThuNgan
+CREATE ROLE Phong_KhaoThi
+GO
+-- grant priviledges to roles
+GRANT SELECT ON DangKy TO HocVien
+
+-- create user
+CREATE USER [HV000006] WITH PASSWORD='H881frGF5OZF'
+GO
+
+-- add role to users
+ALTER ROLE HocVien ADD MEMBER HV000006
+GO
